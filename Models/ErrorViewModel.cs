4c8263a1bf364f927e81bb8369b23ec7e4fff6e8
thuybtd <thuy.btd06551@sinhvien.hoasen.edu.vn>
2025-03-31 15:57:08 +0700

    Initil commit : set up ASP.NET MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMS_Webapp_Firebase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
